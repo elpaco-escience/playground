31e79349d18b071d60599434ead3f811a8db7e87
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2024-06-07 15:23:59 +0200

    fix minor bug
    ggthemes package is required for using theme_tufte()

--- continuers.R
+++ continuers.R
@@ -9,7 +9,7 @@
 
 # packages and useful functions -------------------------------------------
 
-list.of.packages <- c("tidyverse","tidytext")
+list.of.packages <- c("tidyverse","tidytext","ggthemes")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
